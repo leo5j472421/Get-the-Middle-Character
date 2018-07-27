52a5a1de5041895d9052fe07056352c06982d198
Unknown <leo5j472421@yahoo.com.tw>
2018-07-27 17:55:02 +0800

    Good job

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Kata
+    {
+        public static string GetMiddle(string s)
+        {
+            int mid = s.Length / 2;
+            return s.Length % 2 == 1 ? s.Substring(mid, 1) : s.Substring(mid - 1, 2);
+        }
+
+        public static string GetMiddle2(string s)
+        {
+            return s.Substring((s.Length - 1 )/ 2 , (s.Length + 1) % 2 + 1);
+        }
+    }
+}
---
+++ ConsoleApp1Tests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void GetMiddleTest()
+        {
+            Assert.AreEqual("es", Kata.GetMiddle2("test"));
+            Assert.AreEqual("t", Kata.GetMiddle2("testing"));
+            Assert.AreEqual("dd", Kata.GetMiddle2("middle"));
+            Assert.AreEqual("A", Kata.GetMiddle2("A"));
+        }
+    }
+}
